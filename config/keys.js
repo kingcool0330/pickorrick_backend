268b867b3a82d749603447021f8953b6e1709905
moises.guimet <secretsuper.dev@gmail.com>
2023-05-15 06:36:22 +0900

    update db config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
-  mongoURI: "mongodb+srv://cmon:cmon@cmondb.vfrm2t1.mongodb.net/",
+  // mongoURI: "mongodb://localhost:27017/db_pickorrick",
+  mongoURI:
+    "mongodb+srv://secretsuperdev:ZB7XkQFkif12q6Yy@cluster0.nfnorav.mongodb.net/",
   secretOrKey: "secret",
   port: 5000,
 };
